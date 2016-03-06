c1f119d29770c60251157f982a3e4c32e54eb590
sally pang <sallyxpang_@live.com>
2016-03-05 23:08:02 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var userRef = new Firebase('https://dwabit.firebaseio.com/');
+var distressSignals = new Firebase('https://dwabit.firebaseio.com/DistressSignals/');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Security' });
+});
+
+router.get('/users/:id', function(req, res, next) {
+	res.render('user', { title: 'User', id: req.params.id});
+
+});
+
+router.get('/user', function(req, res, next) {
+	res.render('user', { title: 'Meryl'});
+});
+
+module.exports = router;
